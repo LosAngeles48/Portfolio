230a8158d20805096b041d5d47442c615f5588ca
Dmitry48 <Dima_1996.33@mail.ru>
2023-08-19 19:40:26 +0700

    Portfolio-Done

---
+++ src/components/goToBtn/GoToBtn.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from "styled-components";
+import {Icon} from "../icon/Icon";
+
+export const GoToBtn = () => {
+    return (
+        <StyledGoToBtn>
+            <Icon iconId={""}/>
+
+        </StyledGoToBtn>
+    );
+};
+
+const StyledGoToBtn = styled.button`
+`
